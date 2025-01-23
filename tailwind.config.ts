db81010d8503157ef0a381da0e5c3cc0145bbd36
Raskollnikov <bakhalaia56@gmail.com>
2025-01-23 21:51:27 +0400

    initialize app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./utils/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -11,6 +12,12 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        1:'var( --colour--bg-1)',
+        2:'var(--colour--bg-2)',
+        3:'var(--colour--bg-3)',
+        4:'var(--colour--bg-4)',
+        "colour-1":"var(--colour--border-1)",
+        "colour-2":"var(--colour--border-2)"
       },
     },
   },
